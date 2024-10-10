48b8755fc086e3585e0651e5541e247f17695362
Shubham61800 <106237435+Shubham61800@users.noreply.github.com>
2024-10-10 18:10:07 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-between ">
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+      </div>
+    </>
+  )
+}
+
+export default App
